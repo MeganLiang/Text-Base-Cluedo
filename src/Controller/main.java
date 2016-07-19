e31e57c7fd6ba4e411d7c0d2a9b5919019c650aa
Megan <meganliang15@hotmail.com>
2016-07-19 12:20:34 +1200

    deals remaining cards after solution has been randomly generated

--- src/Controller/main.java
+++ src/Controller/main.java
@@ -8,7 +8,9 @@ import model.Weapon;
  * Created by megan on 18/07/16.
  */
 public class main {
-
+    /**
+     * one character, one weapon and one room card are selected at random and is the solution
+     */
     public static void initSolution() {
         Weapon randomWeapon = new Weapon(null);
         Room randomRoom = new Room(null);

1831b85bd6db693a4050d706ae03e4acc2a006d2
Tristan Irons <Tristan Irons>
2016-07-24 11:59:59 +1200

    first commit

--- src/controller/Game.java
+++ src/controller/Game.java
@@ -6,20 +6,106 @@ import view.TextBaseCluedo;
 
 import java.util.*;
 
-import static controller.Setup.chooseCharacters;
-import static controller.Setup.initGame;
-
 public class Game {
-
-    private static Setup setup;
-    public static TextBaseCluedo textBaseCluedo = new TextBaseCluedo();
-
+    private static Solution gameSolution;
+    private static TextBaseCluedo textBaseCluedo = new TextBaseCluedo();
+    private static int numPlayers;
     private static List<Player> playersList = new ArrayList<>();
 
-    public static Set<Weapon.Weapons> availableWeapons;
-    public static List<Room.Rooms> availableRooms;
-    public static Set<Character.Characters> availableCharacters;
+    private static Set<Weapon.Weapons> availableWeapons;
+    private static Set<Room.Rooms> availableRooms;
+    private static Set<Character.Characters> availableCharacters;
 
+    /**
+     * one character, one weapon and one room card are selected at random and is the solution
+     */
+    public static Solution initGame() {
+        availableCharacters = new HashSet<>(Arrays.asList(Character.Characters.values()));
+        availableRooms= new HashSet<>(Arrays.asList(Room.Rooms.values()));
+        availableWeapons = new HashSet<>(Arrays.asList(Weapon.Weapons.values()));
+
+        numPlayers = textBaseCluedo.getNumberOfPlayers();
+
+        Weapon randomWeapon = new Weapon(null);
+        Room randomRoom = new Room(null);
+        Character randomCharacter = new Character(null);
+
+        randomRoom.setRoomName((Room.Rooms) randomRoom.getRandom(Room.Rooms.class));
+        randomWeapon.setWeaponName((Weapon.Weapons) randomWeapon.getRandom(Weapon.Weapons.class));
+        randomCharacter.setCharacterName(randomCharacter.getRandom(Character.Characters.class));
+
+
+        gameSolution = new Solution(randomWeapon, randomRoom, randomCharacter);
+        gameSolution.printSolution();
+        return gameSolution;
+    }
+
+    /**
+     * solution is picked, cards are dealt evenly to players
+     */
+    private static void dealCards() {
+        initGame();
+        deal(Weapon.Weapons.class, Character.Characters.class, Room.Rooms.class);
+        for(Player p: playersList) {
+            System.out.println("Player's hand: ============");
+            p.printHand();
+        }
+    }
+
+    /**
+     * the implementation for dealing the cards
+     * @param weaponClass the num class Weapons
+     * @param characterClass the enum class Characters
+     * @param roomClass the enum class Rooms
+     */
+    private static void deal(Class<Weapon.Weapons> weaponClass, Class<Character.Characters> characterClass, Class<Room.Rooms> roomClass) {
+        int numWeapons = weaponClass.getEnumConstants().length-1; //5, number of available weapons minus the solution weapon
+        int numChars = characterClass.getEnumConstants().length-1;
+        int numRooms = roomClass.getEnumConstants().length-1;
+
+        int numCards = numWeapons+numChars+numRooms;
+
+        int dealtEvenly = numCards/numPlayers;
+        System.out.println("c/p: " + dealtEvenly);
+        List<Weapon.Weapons> weaponsList = new ArrayList<>(Arrays.asList(Weapon.Weapons.values()));
+        weaponsList.remove(gameSolution.getWeapon().getEnum());
+        List<Character.Characters> charactersList = new ArrayList<>(Arrays.asList(Character.Characters.values()));
+        charactersList.remove(gameSolution.getCharacter().getEnum());
+        List<Room.Rooms> roomsList = new ArrayList<>(Arrays.asList(Room.Rooms.values()));
+        roomsList.remove(gameSolution.getRoom().getEnum());
+
+        List<Card> cardsList = new ArrayList<>();
+        for(Weapon.Weapons w: weaponsList) {
+            Weapon weapon = new Weapon(w);
+            cardsList.add(weapon);
+        }
+        for(Character.Characters c: charactersList) {
+            Character character = new Character(c);
+            cardsList.add(character);
+        }
+        for(Room.Rooms r: roomsList) {
+            Room room = new Room(r);
+            cardsList.add(room);
+        }
+        System.out.println("_______________________________________");
+
+        Collections.shuffle(cardsList);
+
+        int index = 0;
+        for(Player player : playersList) {
+            for (int i = 0; i < dealtEvenly; i++) {
+                Card c = cardsList.get(index);
+                player.getHand().addToHand(c);
+                index++;
+            }
+        }
+        System.out.println("Everyone can see: ");
+        for(int i = index; i < cardsList.size(); i++) {
+            System.out.println(cardsList.get(i).getName());
+        }
+
+
+    }
 
     /**
      * player can make accusations to win the game
@@ -64,21 +150,85 @@ public class Game {
         return new Suggestion(w,r,c);
     }
 
+    /**
+     * players are asked to choose a character from the game, which determines their
+     * starting position
+     * @param numOfPlayers
+     * @return Set<Characters></>
+     */
+    private static Set<Character.Characters> chooseCharacters(int numOfPlayers) {
+        int count = 0;
+        Set<Character.Characters> chosenCharacters = new HashSet<>();
+        textBaseCluedo.printHelp();
+        while (count != numOfPlayers) {
 
-    public static Set<Weapon.Weapons> getAvailableWeapons() {
-        return availableWeapons;
+            String playerName = textBaseCluedo.getPlayers();
+            Player player = new Player(playerName);
+            playersList.add(player);
+            player.setName(playerName);
+
+            String next = textBaseCluedo.choosingCharacters();
+            if(next.contains("help")) {
+                textBaseCluedo.printHelp();
+                next = textBaseCluedo.choosingCharacters();
+            }
+            while(chosenCharacters.contains(Character.Characters.fromString(next)) || !availableCharacters.contains(Character.Characters.fromString(next))) { //invalid input, can be duplicate character or not a token
+                next = textBaseCluedo.invalidCharacterInput();
+            }
+            if(!chosenCharacters.contains(Character.Characters.fromString(next))  && availableCharacters.contains(Character.Characters.fromString(next))) {
+                chosenCharacters.add(Character.Characters.valueOf(next));
+                player.setCharacter(Character.Characters.valueOf(next));
+                count++;
+            }
+        }
+        System.out.println("=======================================");
+        return chosenCharacters;
     }
 
-    public static List<Room.Rooms> getAvailableRooms() {
-        return availableRooms;
-    }
+    public void weaponsToRooms(Class<Weapon.Weapons> weaponClass, Class<Room.Rooms> roomClass) {
+        int numWeapons = weaponClass.getEnumConstants().length-1; //5, number of available weapons minus the solution weapon
+        int numRooms = roomClass.getEnumConstants().length-1;
 
-    public static Set<Character.Characters> getAvailableCharacters() {
-        return availableCharacters;
-    }
+        int dealtEvenly = numWeapons/numPlayers;
+        System.out.println("c/p: " + dealtEvenly);
+        List<Weapon.Weapons> weaponsList = new ArrayList<>(Arrays.asList(Weapon.Weapons.values()));
+        weaponsList.remove(gameSolution.getWeapon().getEnum());
+        List<Character.Characters> charactersList = new ArrayList<>(Arrays.asList(Character.Characters.values()));
+        charactersList.remove(gameSolution.getCharacter().getEnum());
+        List<Room.Rooms> roomsList = new ArrayList<>(Arrays.asList(Room.Rooms.values()));
+        roomsList.remove(gameSolution.getRoom().getEnum());
 
-    public static void addToPlayersList(Player player) {
-        playersList.add(player);
+        List<Card> cardsList = new ArrayList<>();
+        for(Weapon.Weapons w: weaponsList) {
+            Weapon weapon = new Weapon(w);
+            cardsList.add(weapon);
+        }
+        for(Character.Characters c: charactersList) {
+            Character character = new Character(c);
+            cardsList.add(character);
+        }
+        for(Room.Rooms r: roomsList) {
+            Room room = new Room(r);
+            cardsList.add(room);
+        }
+        System.out.println("_______________________________________");
+
+        Collections.shuffle(cardsList);
+
+        int index = 0;
+        for(Player player : playersList) {
+            for (int i = 0; i < dealtEvenly; i++) {
+                Card c = cardsList.get(index);
+                player.getHand().addToHand(c);
+                index++;
+            }
+        }
+        System.out.println("Everyone can see: ");
+        for(int i = index; i < cardsList.size(); i++) {
+            System.out.println(cardsList.get(i).getName());
+        }
+
+
     }
 
     /**
@@ -101,47 +251,6 @@ public class Game {
 
     }
 
-    public static void movePlayer() {
-        for(Player player : playersList) {
-            System.out.println("Hi " + player.getName());
-            Random random = new Random();
-            int diceRoll = random.nextInt(6 - 1 + 1) + 1;
-            System.out.println("You rolled a " + diceRoll);
-            String commands = "";
-            List<Move.Moves> moves = new ArrayList<>(Arrays.asList(Move.Moves.values()));
-            if(player.isInRoom()) {
-                System.out.println("You are in a room");
-                commands = textBaseCluedo.movingInRoom();
-                String[] commandArray = commands.trim().split("\\s+");
-                String exitRoute = commandArray[0];
-                for(int i=1; i<commandArray.length; i++) {
-                    while(!moves.contains(Move.Moves.fromString(commandArray[i]))) {
-                        commands = textBaseCluedo.invalidArrayInput();
-                        commandArray = commands.trim().split("\\s+");
-                    }
-                }
-                while(commandArray.length != diceRoll+1) {
-                    commands = textBaseCluedo.invalidArrayInput();
-                    commandArray = commands.trim().split("\\s+");
-                }
-            }else {
-                commands = textBaseCluedo.moving(); //not in room
-                String[] commandArray = commands.trim().split("\\s+");
 
-                for(int i=0; i<commandArray.length; i++) {
-                    while(!moves.contains(Move.Moves.fromString(commandArray[i]))) {
-                        commands = textBaseCluedo.invalidArrayInput();
-                        commandArray = commands.trim().split("\\s+");
-                    }
-                }
-                while(commandArray.length != diceRoll) {
-                    commands = textBaseCluedo.invalidArrayInput();
-                    commandArray = commands.trim().split("\\s+");
-                }
-            }
-
-
-
-        }
     }
 }

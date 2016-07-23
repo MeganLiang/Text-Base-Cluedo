137d143cc83e45f86bc6807165ab05cf8ec01400
Megan <meganliang15@hotmail.com>
2016-07-23 14:03:56 +1200

    on the correct branch

--- src/model/Room.java
+++ src/model/Room.java
@@ -57,26 +57,6 @@ public class Room extends Location implements Card  {
         return roomType;
     }
 
-//    public static final Room[] rooms = new Room[] {
-//            new Room(Rooms.BallRoom),
-//            new Room(Rooms.Conservatory),
-//            new Room(Rooms.Kitchen),
-//            new Room(Rooms.Study),
-//            new Room(Rooms.Hall),
-//            new Room(Rooms.Lounge),
-//            new Room(Rooms.Library),
-//            new Room(Rooms.DiningRoom),
-//            new Room(Rooms.BilliardRoom)
-//    };
-
-//    public static Room getRandom() {
-//        Random r = new Random();
-//        Room room = rooms[r.nextInt(rooms.length)];
-//        return room;
-//
-//    }
-
-
     public static void main(String[] args) {
         getRandom(Rooms.class);
     }
